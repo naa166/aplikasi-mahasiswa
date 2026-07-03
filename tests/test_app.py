b158e415aa2d676ce9f5a609f2ea4a772a0074de
Nabilla Atikah <nabillaatikah95@gmail.com>
2026-07-03 15:39:37 +0700

    Memperbaiki test yang gagal

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,4 +6,4 @@ def test_health():
     client = app.test_client()
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.get_json()["status"] == "sehat"   # sengaja salah, harusnya "healthy"
+    assert response.get_json()["status"] == "healthy"
